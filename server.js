2db63c77929490887cb5df5a8dc370b3cbdef784
Julian <kramerjul@gmail.com>
2019-04-02 23:26:14 +0200

    added whoami api

--- server.js
+++ server.js
@@ -4,6 +4,8 @@
 // init project
 var express = require('express');
 var app = express();
+var dotenv = require('dotenv');
+dotenv.config();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
@@ -18,13 +20,11 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get('/api/whoami', function(req,res){
+  console.log(req.headers);
+  res.json({ipaddress: req.headers['x-forwarded-for'] || req.connection.remoteAddress, language: req.headers['accept-language'], software: req.headers['user-agent']});
 });
-
-
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
